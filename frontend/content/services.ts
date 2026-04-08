93c4f02ee1ae9252dfcaf73ed9c7044923745423
ApolloDev-linx <bossangel1127@gmail.com>
2026-04-07 21:32:23 -0500

    update: added more services to the service section

--- frontend/content/services.ts
+++ frontend/content/services.ts
@@ -5,17 +5,22 @@ export const serviceCategories: ServiceCategory[] = [
   {
     title: "Acrylic",
     items: [
-      { name: "Full set (regular polish)", price: "$35" },
-      { name: "Fill (regular polish)", price: "$25" },
-      { name: "Full set (gel polish)", price: "$45" },
-      { name: "Fill (gel polish)", price: "$35" },
+      { name: "Full set (regular polish)", price: "$35+" },
+      { name: "Fill (regular polish)", price: "$25+" },
+      { name: "Full set (gel polish)", price: "$45+" },
+      { name: "Fill (gel polish)", price: "$35+" },
+      {name: "Full set ombre", price: "$55+"}
     ],
   },
   {
     title: "Pedicure",
     items: [
       { name: "Judah Pedicure", price: "$47" },
-      { name: "Jelly Pedicure", price: "$55" },
+      { name: "Volcano Pedicure", price: "$55" },
+      {name: "Regular Pedicure", price :"$35+"},
+      {name: "Angel Pedicure " , price : "$37+"},
+      {name: "Any Pedicure with gel polish is extra", price : "$15+"}
+
     ],
   },
   {
@@ -23,6 +28,7 @@ export const serviceCategories: ServiceCategory[] = [
     items: [
       { name: "Dry manicure (white color)", price: "$20" },
       { name: "Gel polish", price: "$25" },
+      {name: "Color change toes" , price: "$28"}
     ],
   },
   {
@@ -30,7 +36,22 @@ export const serviceCategories: ServiceCategory[] = [
     items: [
       { name: "Acrylic take off", price: "$12" },
       { name: "Repair", price: "$3+" },
+      {name: "Any Design" , price : "$5+"},
+      {name: "3D Designs" , price: "$15+"}
     ],
   },
+{
+title: "Eye Lashes Extensions",
+items: [
+	{name: "Hawaiian Style", price: "$90"},
+	{name: "Russian Volume", price: "$110"},
+	{name: "Cat Eye Style", price: "$100"},
+	{name: "Natural Style " ,price: "$80"},
+	{name: "Remove Eye Lashes" , price: "$10" }
+],
+},
+
+
+
 ];
 
